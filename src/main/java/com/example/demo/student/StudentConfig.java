520221b85b4ca0cc6f3fc7b8b36ff1f2d4f09a4a
goulkan <landongkista@email.unikom.ac.id>
2021-07-01 15:00:46 +0700

    spring jpa

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student("mariam", "mariam@gmail.com", LocalDate.of(2000, Month.APRIL, 5));
+            Student alex = new Student("alex", "alex@gmail.com", LocalDate.of(2010, Month.AUGUST, 5));
+            repository.saveAll(
+                    List.of(mariam, alex)
+            );
+        };
+    }
+}
